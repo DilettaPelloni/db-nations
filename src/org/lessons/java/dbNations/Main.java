28b15b427ef152947953a103596220aa26a7f644
Diletta Pelloni <dilpel@live.it>
2023-06-16 17:10:46 +0200

    Mostrate lingue dato un id

--- src/org/lessons/java/dbNations/Main.java
+++ src/org/lessons/java/dbNations/Main.java
@@ -23,6 +23,8 @@ public class Main {
         //apro la connessione
         try(Connection c = DriverManager.getConnection(url, user, password)) {
 
+            //------------------------- PRIMA QUERY: TABELLA FILTRATA PER NOME NAZIONE -------------------------
+
             //chiedo all'utente la stringa per il filtro
             System.out.println("Ricerca: ");
             String filter = scan.nextLine();
@@ -40,11 +42,9 @@ public class Main {
             //creo lo statement passando la mia query
             try(PreparedStatement ps = c.prepareStatement(query)) {
 
-                //setto il valore ? della query
-                ps.setString(1, "%" + filter + "%");
+                ps.setString(1, "%" + filter + "%"); //setto il valore ? della query
 
-                //eseguo la query e uso il ResultSet
-                try(ResultSet rs = ps.executeQuery()) {
+                try(ResultSet rs = ps.executeQuery()) { //eseguo la query e uso il ResultSet
 
                     //----- INTESTAZIONE TABELLA -----
                     int colNum = rs.getMetaData().getColumnCount(); //numero di colonne della tabella risultato
@@ -77,6 +77,88 @@ public class Main {
 
                 }
             }
+
+            //------------------------- SECONDA QUERY: RICERCA INFO PER ID -------------------------
+
+            //chiedo all'utente l'id e verifico che mi dia un numero
+            Integer choosenId = null;
+            do {
+                System.out.println("Scegli l'id di una nazione: ");
+                try {
+                    choosenId = Integer.parseInt(scan.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Devi scrivere un numero");
+                }
+            }while(choosenId == null);
+
+            //definisco la query che otterrà la nazione
+            String countryQuery = """
+                    SELECT `countries`.`name`
+                    FROM `countries`
+                    WHERE `countries`.`country_id` = ?;
+            """;
+
+            String countryName = null;
+
+            try(PreparedStatement ps = c.prepareStatement(countryQuery)) {
+                ps.setInt(1, choosenId); //setto il valore ? della query
+                try(ResultSet rs = ps.executeQuery()) { //eseguo la query e uso il ResultSet
+                    while (rs.next()) {
+                        countryName = rs.getString("name");
+                    }
+                }
+            }
+
+            if(countryName != null) { //se mi è stato dato un id valido
+
+                System.out.println("Dettagli della nazione: " + countryName); //stampo la nazione
+
+                //definisco la query che otterrà le lingue
+                String languageQuery = """
+                        SELECT `countries`.`name` AS `country_name`, `languages`.`language`
+                        FROM `countries`
+                        JOIN `country_languages` ON `countries`.`country_id` = `country_languages`.`country_id`
+                        JOIN `languages` ON `country_languages`.`language_id` = `languages`.`language_id`
+                        WHERE `countries`.`country_id` = ?;
+                """;
+
+                try(PreparedStatement ps = c.prepareStatement(languageQuery)) {
+
+                    ps.setInt(1, choosenId); //setto il valore ? della query
+                    try(ResultSet rs = ps.executeQuery()) { //eseguo la query e uso il ResultSet
+                        List<String> languages = new ArrayList<>(); //lista che conterrà le lingue
+                        while (rs.next()) {
+                            String language = rs.getString("language");
+                            languages.add(language);//aggiungo la lingua alla lista
+                        }
+                        String langString = "Lingue: "; //preparo la stringa da mostrare
+                        for (int i = 0; i < languages.size(); i++) { //tutto 'sto casino per le virgole (╯°□°）╯︵ ┻━┻
+                            langString += languages.get(i);
+                            if(i < languages.size() - 1) {
+                                langString += ", "; //se non sono all'ultimo elemento della lista metto la virgola
+                            }
+                        }
+                        System.out.println(langString);
+                    }
+                }
+
+
+            } else {
+                System.out.println("L'id " + choosenId + " non è valido");
+            }
+
+
+            //definisco la query che otterrà le statistiche più recenti
+//            String statisticQuery = """
+//                   SELECT `countries`.`name`, `country_stats`.`year`, `country_stats`.`population`, `country_stats`.`gdp`
+//                   FROM `countries`
+//                   JOIN `country_stats` ON `countries`.`country_id` = `country_stats`.`country_id`
+//                   WHERE `countries`.`country_id` = ?
+//                   ORDER BY `country_stats`.`year` DESC
+//                   LIMIT 1;
+//            """;
+
+
 
         }catch (SQLException e) {  //in caso il collegamento non abbia funzionato
             System.out.println("Errore durante il collegamento al DB");
