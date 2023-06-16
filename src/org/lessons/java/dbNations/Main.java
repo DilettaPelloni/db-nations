162fcf1c03a268ed2d88ccb09bc50a4f370a5422
Diletta Pelloni <dilpel@live.it>
2023-06-16 15:34:18 +0200

    Implementata grafica con tabella

--- src/org/lessons/java/dbNations/Main.java
+++ src/org/lessons/java/dbNations/Main.java
@@ -1,7 +1,12 @@
 package org.lessons.java.dbNations;
 
 //IMPORT
+import com.bethecoder.ascii_table.ASCIITable;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -19,9 +24,8 @@ public class Main {
         try(Connection c = DriverManager.getConnection(url, user, password)) {
 
             //chiedo all'utente la stringa per il filtro
-            System.out.println("Filtra: ");
+            System.out.println("Ricerca: ");
             String filter = scan.nextLine();
-
 
             //definisco la query
             String query = """
@@ -41,20 +45,36 @@ public class Main {
 
                 //eseguo la query e uso il ResultSet
                 try(ResultSet rs = ps.executeQuery()) {
-                    while(rs.next()) { //per ogni riga del ResultSet
-                        //salvo i valori delle colonne
+
+                    //----- INTESTAZIONE TABELLA -----
+                    int colNum = rs.getMetaData().getColumnCount(); //numero di colonne della tabella risultato
+                    String[] tableHeader = new String[colNum]; //array che conterrà le etichette delle colonne
+                    for (int i = 0; i < colNum; i++) { //tante volte quante sono le colonne
+                        tableHeader[i] = rs.getMetaData().getColumnLabel(i + 1); //inserisco nell'array l'intestazione della colonna
+                    }
+
+                    //----- RIGHE TABELLA -----
+                    List<String[]> tableRowsList = new ArrayList<>(); //lista che conterrà un array di stringhe per ogni riga (poi la converto)
+
+                    while (rs.next()) { //per ogni riga del risultato
+                        //raccolgo i dati della riga
                         String countryName = rs.getString("country_name");
                         int countryId = rs.getInt("country_id");
                         String regionName = rs.getString("region_name");
                         String continentName = rs.getString("continent_name");
-                        //stampo i valori
-                        System.out.println(
-                                    "Nazione: " + countryName +
-                                    " - Id Nazione: " + countryId +
-                                    " - Regione: " + regionName +
-                                    " - Continente: " + continentName
-                                );
+
+                        //creo un array con i dati della riga
+                        String[] row = {countryName, String.valueOf(countryId), regionName, continentName};
+
+                        //aggiungo row alla lista
+                        tableRowsList.add(row);
                     }
+
+                    //----- STAMPO TABELLA -----
+                    String[][] tableRows = tableRowsList.toArray(new String[0][]);
+                    String table = ASCIITable.getInstance().getTable(tableHeader, tableRows);
+                    System.out.println(table);
+
                 }
             }
 
